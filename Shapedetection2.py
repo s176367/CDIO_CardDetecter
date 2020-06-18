603d6b55eab0a30e5bd32ccc9550f142c1263be9
Mads Geertsen <42939172+s176367@users.noreply.github.com>
2020-06-18 13:05:04 +0200

    Korrekte templates nu lavet og der kan detectes på dem

--- Shapedetection2.py
+++ Shapedetection2.py
@@ -43,38 +43,6 @@ threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
 counter = 0
 
 
-def stackImages(scale, imgArray):  # metode herfra https://www.murtazahassan.com/real-time-contours-shape-detection/
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range(0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
-                                                None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank] * rows
-        hor_con = [imageBlank] * rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor = np.hstack(imgArray)
-        ver = hor
-    return ver
-
 
 def getContours(img, imgContour, standardimg):
     global counter, string, contours
@@ -105,9 +73,6 @@ def getContours(img, imgContour, standardimg):
                     approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
                     box = np.int0(approx)
 
-
-
-
                     if len(approx) == 4:
                         counter = counter + 1
                         warpPic = standardimg.copy()
@@ -135,10 +100,11 @@ def warpPicture(botRight, botLeft, topRight, topLeft, img):
     output = cv2.warpPerspective(img, matrix, (width, height))
     #checkAfAlle(output)
     print(str(counter))
-    #print(checkAfAlle(output))
-    # cv2.imshow(''+ str(counter), output)
-    cv2.imwrite('warpedpicture' + str(counter) + '.jpg', output)
-    #print('warpedPicture' + str(counter))
+    print(checkAfAlle(output))
+    cv2.imshow(''+ str(counter), output)
+
+    #cv2.imwrite('warpedpicture' + str(counter+381) + '.jpg', output)
+    #print('warpedPicture' + str(counter+266))
 
 
 def antiflip(box):
@@ -168,14 +134,14 @@ def antiflip(box):
 def checkAfkort (img, template):
     img1 = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     template1 = cv2.cvtColor(template, cv2.COLOR_RGB2GRAY)
-    # cv2.imshow('tresh1', img1)
-    # cv2.imshow('tresh2', template1)
+    #cv2.imshow('tresh1', img1)
+    #cv2.imshow('tresh2', template1)
 
-    ret,thresh1 = cv2.threshold(img1, 190, 230, cv2.THRESH_BINARY)
-    ret,thresh11 = cv2.threshold(template1, 190, 230, cv2.THRESH_BINARY)
+    ret,thresh1 = cv2.threshold(img1, 170, 230, cv2.THRESH_BINARY)
+    ret,thresh11 = cv2.threshold(template1, 170, 230, cv2.THRESH_BINARY)
     bitwise = cv2.bitwise_xor(thresh1, thresh11)
-    # cv2.imshow('tresh1',thresh1)
-    # cv2.imshow('tresh2', thresh11)
+    cv2.imshow('tresh1',thresh1)
+    #cv2.imshow('tresh2', thresh11)
     # cv2.imshow('bitwise',bitwise)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
--- manuelBit.py
+++ manuelBit.py
@@ -2,22 +2,22 @@ import cv2
 
 
 
-img = cv2.imread('test2.jpg', cv2.IMREAD_GRAYSCALE)
-template = cv2.imread('templateCards/warpedPicture3.jpg', cv2.IMREAD_GRAYSCALE)
-template1 = cv2.imread('templateCards/warpedPicture4.jpg', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('templateCards/warpedpicture375.jpg', cv2.IMREAD_GRAYSCALE)
+template = cv2.imread('templateCards/warpedpicture166.jpg', cv2.IMREAD_GRAYSCALE)
+template1 = cv2.imread('templateCards/warpedpicture377.jpg', cv2.IMREAD_GRAYSCALE)
 
 cv2.imshow('template', template)
 
-ret,threshpic8 = cv2.threshold(img,190, 255,cv2.THRESH_BINARY)
-ret,threshpic7 = cv2.threshold(template, 190, 255, cv2.THRESH_BINARY)
-ret,threshpic153 = cv2.threshold(template1,190, 255,cv2.THRESH_BINARY)
+ret,threshpic8 = cv2.threshold(img,170, 255,cv2.THRESH_BINARY)
+ret,threshpic7 = cv2.threshold(template, 170, 255, cv2.THRESH_BINARY)
+ret,threshpic153 = cv2.threshold(template1,170, 255,cv2.THRESH_BINARY)
 
-cv2.imshow('162',threshpic7)
-cv2.imshow('92',threshpic8)
-cv2.imshow('ace',threshpic153)
+cv2.imshow('template',threshpic7)
+cv2.imshow('img',threshpic8)
+cv2.imshow('testpicture',threshpic153)
 
 bitwise = cv2.bitwise_xor(threshpic8, threshpic7)
-bitwise1 = cv2.bitwise_xor(threshpic8, threshpic153)
+bitwise1 = cv2.bitwise_xor(threshpic7, threshpic153)
 
 
 cv2.imshow('Bitwise xor', bitwise)
